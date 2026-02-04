ae0a58696a3f385776a3c42663757dcc9acc25c2
BrayanAgudelo1423 <brayan.campino56596@ucaldas.edu.co>
2026-02-03 13:26:24 -1200

    Update site

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
+  basePath: "/urlaty",
   images: {
     unoptimized: true,
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-      },
-    ],
   },
 };
 
