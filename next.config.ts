b3c79088c92469d1e90b825f218b154ed7d5ad5f
BrayanAgudelo1423 <brayan.campino56596@ucaldas.edu.co>
2026-02-04 19:37:46 -1200

    Fix: Agregar basePath para GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: "/urlaty",
+  assetPrefix: "/urlaty",
   
   images: {
     unoptimized: true,
